8a43ba247afb5eb2d739bc64d7152b5420416c03
meto1558 <metehanaytas1997@gmail.com>
2025-07-21 02:09:12 +0300

    fix(MixinScreen): Fixed background display issue on game pause

--- src/main/java/com/manifestors/shinrai/mixins/minecraft/client/gui/screen/MixinScreen.java
+++ src/main/java/com/manifestors/shinrai/mixins/minecraft/client/gui/screen/MixinScreen.java
@@ -3,17 +3,16 @@ package com.manifestors.shinrai.mixins.minecraft.client.gui.screen;
 import com.manifestors.shinrai.client.utils.rendering.BackgroundDrawer;
 import net.minecraft.client.gui.DrawContext;
 import net.minecraft.client.gui.screen.Screen;
-import net.minecraft.util.Identifier;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(Screen.class)
-public class MixinScreen {
+public abstract class MixinScreen {
 
-    @Inject(method = "renderBackgroundTexture", at = @At("HEAD"), cancellable = true)
-    private static void showShinraiBackground(DrawContext context, Identifier texture, int x, int y, float u, float v, int width, int height, CallbackInfo ci) {
+    @Inject(method = "renderPanoramaBackground", at = @At(value = "HEAD"), cancellable = true)
+    private void showShinraiBackground(DrawContext context, float deltaTicks, CallbackInfo ci) {
         BackgroundDrawer.drawBackgroundTexture(context);
         ci.cancel();
     }
