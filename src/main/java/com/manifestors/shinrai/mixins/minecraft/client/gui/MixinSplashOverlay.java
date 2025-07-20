075ee09efd7978584799f9264b7e1195a8513060
meto1558 <metehanaytas1997@gmail.com>
2025-07-20 20:14:42 +0300

    refactor(MixinSplashOverlay): New splash screen code

--- src/main/java/com/manifestors/shinrai/mixins/minecraft/client/gui/MixinSplashOverlay.java
+++ src/main/java/com/manifestors/shinrai/mixins/minecraft/client/gui/MixinSplashOverlay.java
@@ -1,6 +1,9 @@
 package com.manifestors.shinrai.mixins.minecraft.client.gui;
 
+import com.mojang.blaze3d.pipeline.RenderPipeline;
 import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.gl.RenderPipelines;
+import net.minecraft.client.gui.DrawContext;
 import net.minecraft.client.gui.screen.SplashOverlay;
 import net.minecraft.client.resource.metadata.TextureResourceMetadata;
 import net.minecraft.client.texture.NativeImage;
@@ -18,17 +21,16 @@ import java.io.InputStream;
 
 @Mixin(SplashOverlay.class)
 public class MixinSplashOverlay {
+
+    /* We don't know how it works
     @Unique
     private static final Identifier manifestorsLogo = Identifier.of("shinrai","srclogo2.png");
-
-
 
     @Redirect(method = "<clinit>",at = @At(value = "INVOKE", target = "Lnet/minecraft/util/math/ColorHelper;getArgb(IIII)I",ordinal = 0))
     private static int manifestorsBlack(int alpha, int red, int green, int blue){
 
         return ColorHelper.getArgb(0,0,0);
     }
-
 
     @Redirect(method = "init",at = @At(value = "FIELD", target = "Lnet/minecraft/client/gui/screen/SplashOverlay;LOGO:Lnet/minecraft/util/Identifier;",ordinal = 0))
     private static Identifier init(){
@@ -37,15 +39,16 @@ public class MixinSplashOverlay {
     @Redirect(method = "<clinit>",at = @At(value = "INVOKE", target = "Lnet/minecraft/util/Identifier;ofVanilla(Ljava/lang/String;)Lnet/minecraft/util/Identifier;",ordinal = 0))
     private static Identifier manifestorsLogoFun(String path){
         return Identifier.of("shinrai","textures/mddf.png");
+    }*/
+
+    // New code
+    @Redirect(method = "render", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/DrawContext;drawTexture(Lcom/mojang/blaze3d/pipeline/RenderPipeline;Lnet/minecraft/util/Identifier;IIFFIIIIIII)V", ordinal = 0))
+    private void changeSplash(DrawContext instance, RenderPipeline pipeline, Identifier sprite, int x, int y, float u, float v, int width, int height, int regionWidth, int regionHeight, int textureWidth, int textureHeight, int color) {
+        instance.drawTexture(RenderPipelines.GUI_TEXTURED, Identifier.of("shinrai", "lg.png"), MinecraftClient.getInstance().getWindow().getScaledWidth() / 2, 0, 0, 0, 256, 64, 256, 64);
     }
 
-    @Unique
-
-    private static TextureContents loadContents(ResourceManager resourceManager) throws IOException {
-        ResourceFactory resourceFactory = MinecraftClient.getInstance().getDefaultResourcePack().getFactory();
-
-        try (InputStream inputStream = resourceFactory.open(SplashOverlay.LOGO)) {
-            return new TextureContents(NativeImage.read(inputStream), new TextureResourceMetadata(true, true));
-        }
+    @Redirect(method = "render", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/DrawContext;drawTexture(Lcom/mojang/blaze3d/pipeline/RenderPipeline;Lnet/minecraft/util/Identifier;IIFFIIIIIII)V", ordinal = 1))
+    private void testDraw2(DrawContext instance, RenderPipeline pipeline, Identifier sprite, int x, int y, float u, float v, int width, int height, int regionWidth, int regionHeight, int textureWidth, int textureHeight, int color) {
+        instance.drawTexture(RenderPipelines.GUI_TEXTURED, Identifier.of("shinrai", "lg.png"), MinecraftClient.getInstance().getWindow().getScaledWidth() / 2, 0, 0, 0, 256, 64, 256, 64);
     }
 }
