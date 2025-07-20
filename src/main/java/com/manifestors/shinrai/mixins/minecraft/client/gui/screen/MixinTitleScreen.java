ecc8815ad6413c83198c7a6e6c2546c699cd8d6d
meto1558 <metehanaytas1997@gmail.com>
2025-07-21 01:10:28 +0300

    chore: Removed unused imports (again)

--- src/main/java/com/manifestors/shinrai/mixins/minecraft/client/gui/screen/MixinTitleScreen.java
+++ src/main/java/com/manifestors/shinrai/mixins/minecraft/client/gui/screen/MixinTitleScreen.java
@@ -1,6 +1,5 @@
 package com.manifestors.shinrai.mixins.minecraft.client.gui.screen;
 
-import com.manifestors.shinrai.client.Shinrai;
 import com.manifestors.shinrai.client.utils.rendering.BackgroundDrawer;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.gui.DrawContext;
@@ -11,7 +10,6 @@ import net.minecraft.text.Text;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
-import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.Redirect;
--- src/main/java/com/manifestors/shinrai/mixins/minecraft/client/network/MixinClientPlayerEntity.java
+++ src/main/java/com/manifestors/shinrai/mixins/minecraft/client/network/MixinClientPlayerEntity.java
@@ -1,9 +1,7 @@
 package com.manifestors.shinrai.mixins.minecraft.client.network;
 
-import com.llamalad7.mixinextras.injector.ModifyReturnValue;
 import com.manifestors.shinrai.client.Shinrai;
 import com.manifestors.shinrai.client.event.events.TickEvent;
-import com.manifestors.shinrai.client.module.modules.visuals.AntiBlind;
 import net.minecraft.client.network.ClientPlayerEntity;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -16,11 +14,6 @@ public class MixinClientPlayerEntity {
     @Inject(method = "tickMovement", at = @At(value = "HEAD"))
     private void updateEventHook(CallbackInfo ci) {
         Shinrai.eventManager.listenEvent(new TickEvent());
-    }
-
-    @ModifyReturnValue(method = "isBlind", at = @At(value = "RETURN"))
-    private boolean removeBlindnessEffect(boolean original) {
-        return AntiBlind.INSTANCE.getEnabled() || original;
     }
 
 }
