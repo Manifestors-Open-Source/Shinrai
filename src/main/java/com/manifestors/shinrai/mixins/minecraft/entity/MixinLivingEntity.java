0abbf801cccd44ae0b24a8347960c361519dc131
meto1558 <metehanaytas1997@gmail.com>
2025-07-26 23:08:31 +0300

    refactor(NoJumpDelay):

--- src/main/java/com/manifestors/shinrai/mixins/minecraft/entity/MixinLivingEntity.java
+++ src/main/java/com/manifestors/shinrai/mixins/minecraft/entity/MixinLivingEntity.java
@@ -1,28 +1,21 @@
 package com.manifestors.shinrai.mixins.minecraft.entity;
 
+import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
 import com.manifestors.shinrai.client.Shinrai;
-import com.manifestors.shinrai.client.module.modules.extras.NoPortalCooldown;
 import com.manifestors.shinrai.client.module.modules.movement.NoJumpDelay;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.entity.LivingEntity;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(LivingEntity.class)
 public class MixinLivingEntity {
 
-    @Inject(method = "tickMovement", at = @At("TAIL"))
-    private void onTickMovement(CallbackInfo ci) {
-        if (Shinrai.INSTANCE.getModuleManager().isModuleEnabled(NoJumpDelay.class)) {
-            ((MixinLivingEntityAccessor) (Object) this).setJumpingCooldown(0);
-
-
-        }
-        LivingEntity entity = MinecraftClient.getInstance().player;
-        MixinLivingEntityAccessor accessor = (MixinLivingEntityAccessor)(Object) entity;
-        int currentCooldown = accessor.getJumpingCooldown();
-        System.out.println("JumpingCooldown: " + currentCooldown);
+    @ModifyExpressionValue(method = "tickMovement", at = @At(value = "FIELD", target = "Lnet/minecraft/entity/LivingEntity;jumpingCooldown:I", ordinal = 3))
+    private int removeJumpCooldown(int original) {
+        var entity = (LivingEntity) (Object) this;
+        return Shinrai.INSTANCE.getModuleManager().isModuleEnabled(NoJumpDelay.class) &&
+                entity == MinecraftClient.getInstance().player ? 0 : original;
     }
+
 }
