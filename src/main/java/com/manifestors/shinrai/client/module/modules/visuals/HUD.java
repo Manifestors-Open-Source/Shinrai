5b9f80a733eb2952f880afc210bf84bd93dd8a25
meto1558 <metehanaytas1997@gmail.com>
2025-10-02 12:19:53 +0300

    feat(HUD): Custom status effects render layer

--- src/main/java/com/manifestors/shinrai/client/module/modules/visuals/HUD.java
+++ src/main/java/com/manifestors/shinrai/client/module/modules/visuals/HUD.java
@@ -6,6 +6,11 @@ import com.manifestors.shinrai.client.event.events.rendering.Rendering2DEvent;
 import com.manifestors.shinrai.client.module.Module;
 import com.manifestors.shinrai.client.module.ModuleCategory;
 import com.manifestors.shinrai.client.module.annotations.ModuleData;
+import net.minecraft.entity.effect.StatusEffectInstance;
+import net.minecraft.entity.effect.StatusEffectUtil;
+import net.minecraft.screen.ScreenTexts;
+import net.minecraft.text.MutableText;
+import net.minecraft.text.Text;
 
 import java.awt.*;
 
@@ -18,10 +23,16 @@ public class HUD extends Module {
 
     @ListenEvent
     public void onRendering2D(Rendering2DEvent event) {
-        var context = event.getContext();
+        renderWatermark(event);
+        renderArrayList(event);
+        renderPotionEffects(event);
+    }
 
-        context.drawText(mc.textRenderer, Shinrai.NAME, 2, 3, Color.RED.getRGB(), true);
+    private void renderWatermark(Rendering2DEvent event) {
+        event.getContext().drawText(mc.textRenderer, Shinrai.NAME, 2, 3, Color.RED.getRGB(), true);
+    }
 
+    private void renderArrayList(Rendering2DEvent event) {
         int yOffset = 0;
 
         var activeModules = Shinrai.INSTANCE.getModuleManager().getModules().stream()
@@ -31,10 +42,51 @@ public class HUD extends Module {
 
         for (Module module : activeModules) {
             int x = event.getWidth() - mc.textRenderer.getWidth(module.getName()) - 3;
-            context.drawText(mc.textRenderer, module.getName(), x, yOffset + 5, Color.RED.getRGB(), true);
+            event.getContext().drawText(mc.textRenderer, module.getName(), x, yOffset + 5, Color.RED.getRGB(), true);
 
             yOffset += 11;
         }
+    }
+
+    private void renderPotionEffects(Rendering2DEvent event) {
+        var statusEffects = mc.player.getStatusEffects().stream().sorted((e1, e2) ->
+                        mc.textRenderer.getWidth(e2.getEffectType().value().getName()) - mc.textRenderer.getWidth(e1.getEffectType().value().getName()))
+                .toList();
+        int statusCount = 0;
+        if (!statusEffects.isEmpty() && (mc.currentScreen == null || !mc.currentScreen.showsStatusEffects())) {
+            int yOffset = 0;
+            for (var effectInst : statusEffects) {
+                var effectName = getStatusEffectDescription(effectInst).getString() + " : " +
+                        StatusEffectUtil.getDurationText(effectInst, 1.0F, mc.world.getTickManager().getTickRate()).getLiteralString();
+
+                int x = event.getWidth() - mc.textRenderer.getWidth(effectName) - 3;
+                int y = event.getHeight() - 15;
+
+                event.getContext().drawText(mc.textRenderer, effectName, x, y - yOffset, getRandomColor(statusCount), true);
+                statusCount++;
+                yOffset += 11;
+            }
+        }
+    }
+
+    private Text getStatusEffectDescription(StatusEffectInstance statusEffect) {
+        MutableText text = statusEffect.getEffectType().value().getName().copy();
+        if (statusEffect.getAmplifier() >= 1 && statusEffect.getAmplifier() <= 9) {
+            MutableText spacing = text.append(ScreenTexts.SPACE);
+            int statusEffectAmplifier = statusEffect.getAmplifier();
+            spacing.append(Text.translatable("enchantment.level." + (statusEffectAmplifier + 1)));
+        }
+
+        return text;
+    }
+
+    private int getRandomColor(int index) {
+        Color[] colors = new Color[]{Color.RED, Color.BLUE, Color.ORANGE, Color.PINK, Color.YELLOW, Color.MAGENTA, Color.CYAN, new Color(30, 190, 120)};
+
+        if (index > colors.length - 1)
+            index = 0;
+
+        return colors[index].getRGB();
     }
 
 }
--- src/main/java/com/manifestors/shinrai/mixins/minecraft/gui/hud/MixinInGameHud.java
+++ src/main/java/com/manifestors/shinrai/mixins/minecraft/gui/hud/MixinInGameHud.java
@@ -10,7 +10,9 @@ import net.minecraft.client.gui.hud.InGameHud;
 import net.minecraft.client.render.RenderTickCounter;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.ShieldItem;
+import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
@@ -18,9 +20,18 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 @Mixin(InGameHud.class)
 public class MixinInGameHud {
 
+    @Shadow
+    @Final
+    private MinecraftClient client;
+
     @Inject(method = "render", at = @At(value = "FIELD", target = "Lnet/minecraft/client/option/GameOptions;hudHidden:Z", shift = At.Shift.AFTER))
     private void renderEventHook(DrawContext context, RenderTickCounter tickCounter, CallbackInfo ci) {
-        Shinrai.INSTANCE.getEventManager().listenEvent(new Rendering2DEvent(context, MinecraftClient.getInstance().getWindow().getScaledWidth(), MinecraftClient.getInstance().getWindow().getScaledHeight()));
+        Shinrai.INSTANCE.getEventManager().listenEvent(new Rendering2DEvent(context, this.client.getWindow().getScaledWidth(), this.client.getWindow().getScaledHeight()));
+    }
+
+    @Inject(method = "renderStatusEffectOverlay", at = @At("HEAD"), cancellable = true)
+    private void rewritePotionEffectStatus(DrawContext context, RenderTickCounter tickCounter, CallbackInfo ci) {
+        ci.cancel();
     }
 
     @ModifyExpressionValue(at = @At(value = "INVOKE", target = "Lnet/minecraft/entity/player/PlayerEntity;getOffHandStack()Lnet/minecraft/item/ItemStack;"), method = "renderHotbar")
@@ -33,4 +44,3 @@ public class MixinInGameHud {
     }
 
 }
-
