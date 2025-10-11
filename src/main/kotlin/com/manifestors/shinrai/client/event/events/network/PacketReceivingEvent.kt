b9d84e7fb3292b9ace2591649bc2092b4c7adf65
meto1558 <metehanaytas1997@gmail.com>
2025-10-11 23:16:24 +0300

    refactor: Changed Gradle language and packages

--- src/main/kotlin/com/manifestors/shinrai/client/event/events/network/PacketReceivingEvent.kt
+++ src/main/kotlin/com/manifestors/shinrai/client/event/events/network/PacketReceivingEvent.kt
@@ -1,8 +1,6 @@
 package com.manifestors.shinrai.client.event.events.network
 
 import com.manifestors.shinrai.client.event.CancellableEvent
-import lombok.AllArgsConstructor
-import lombok.Getter
 import net.minecraft.network.packet.Packet
 
 class PacketReceivingEvent(
--- src/main/kotlin/com/manifestors/shinrai/client/event/events/player/ChatMessageSendEvent.kt
+++ src/main/kotlin/com/manifestors/shinrai/client/event/events/player/ChatMessageSendEvent.kt
@@ -1,8 +1,6 @@
 package com.manifestors.shinrai.client.event.events.player
 
 import com.manifestors.shinrai.client.event.CancellableEvent
-import lombok.AllArgsConstructor
-import lombok.Getter
 
 class ChatMessageSendEvent(
     val message: String
